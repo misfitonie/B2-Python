75c58633ad68e9c35c3a3d89204ba61e9fe41de2
misfitonie <35459945+misfitonie@users.noreply.github.com>
2018-11-06 09:44:37 +0100

    update 1.a

--- 1a-add.py
+++ 1a-add.py
@@ -4,14 +4,19 @@
 # FERREIRA Théo
 # 06/11/2018
 
+#fonction vérification d'entrer de chiffre
 def convertInt(param):
     try:
         return int(param)
     except ValueError:
         print("Vous n'avez pas entré un nombre !")
         exit()
-
+        
+  #entrer du nombre 1
 nb1 = convertInt(input('Veuillez entrer un nombre.\n'))
+
+  #entrer du nombre 2
 nb2 = convertInt(input('Veuillez un second nombre.\n'))
 
+  #fonction d'addition 
 print("Voilà le résultat : " + str(nb1 + nb2))
